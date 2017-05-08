34c0e56e0023cfe5e74ae630f20a7d3f9e722229
yorgos_k <georgis.kakavas@gmail.com>
2017-05-08 22:58:40 +0300

    Prep app for Heroku.

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) { 
-  if (req.headers['x-forwarded-proto'] === http) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + res.hostname + req.url);
   }
-})
+});
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log("Express server is up on PORT " + PORT);
+  console.log('Express server is up on PORT ' + PORT);
 });
