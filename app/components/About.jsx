c6ec635bfd5c036031c5a3540eaec37a5fcd9847
yorgos_k <georgis.kakavas@gmail.com>
2017-05-08 23:59:50 +0300

    Edited about and examples pages.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+  	<div>
+      <h3>About the App</h3>
+      <p>This app was made with webpack, node, react, react-router, axios, openweathermap api and foundation. Mostly....</p>
+    </div>
   )
 };
 
