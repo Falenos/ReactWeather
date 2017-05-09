3dba01a77016517a6ef43823722cde1d0c8216d9
yorgos_k <georgis.kakavas@gmail.com>
2017-05-09 18:02:17 +0300

    Fixed example page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,7 +6,7 @@ var About = (props) => {
       <h3>About the App</h3>
       <p>This app was made with webpack, node, react, react-router, axios, openweathermap api and foundation. Mostly....</p>
     </div>
-  )
+  );
 };
 
 module.exports = About;
